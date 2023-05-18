0a2bc677236e8c398d3f2fcc0d3d2023fffd855e
Kasper <kasperkh.kh@gmail.com>
2023-05-19 00:08:22 +0200

    Update setup

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,18 @@ import { defineConfig } from 'vite'
 import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-	root: './src',
-	base: './', // use relative paths
-	publicDir: '../public',
-	clearScreen: false,
-	server: {
-		port: 8000,
-		strictPort: true,
-	},
-	build: {
-		outDir: '../build',
-		emptyOutDir: true,
-		minify: false,
-		sourcemap: true,
-		target: ['chrome64', 'edge79', 'firefox62', 'safari11.1'],
-	},
-	plugins: [
-		svelte({
-			preprocess: vitePreprocess(),
-		}),
-	],
+  clearScreen: false,
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
+  build: {
+    sourcemap: true,
+    target: ['chrome64', 'edge79', 'firefox62', 'safari11.1'],
+  },
+  plugins: [
+    svelte({
+      preprocess: vitePreprocess(),
+    }),
+  ],
 })
